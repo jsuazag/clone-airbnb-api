753940eb8b888153adf4ca56300743d65ce37c54
Jerson Suaza <jerson.suaza@gmail.com>
2020-10-21 14:22:19 -0500

    base route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
-const port = 3000
+const port = process.env.PORT || 8000;
 
 // mongo connection
 require('./connection/connectionDb')
